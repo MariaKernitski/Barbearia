fff1cbe82c151909ff96d4bf57baabc281736db3
Maria <kernitskimaria@gmail.com>
2024-08-28 09:57:01 -0300

    feat cep

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3500;
 
+const cep_endereco = require("./middlewares/cep_endereco.js");
+
 const atendimento_controller = require("./controller/atendimento.js");
 const barbearia_controller = require("./controller/barbearia.js");
 const barbeiro_controller = require("./controller/barbeiro.js");
@@ -11,32 +13,15 @@ const servico_controller = require("./controller/servico.js");
 const usuário_controller = require("./controller/usuário.js");
 
 app.use(express.json());
+app.use(cep_endereco);
 
-app.post("/atendimento", (req, res) => {
-    const atendimento = req.body;
-    const code = atendimento_controller.store(atendimento);
-    res.status(code).json();
-})
-
-app.get("/atendimento", (req, res) => {
-    const atendimentos = atendimento_controller.index();
-    res.json(atendimentos);
-})
-
-app.get("/atendimento/:id", (req, res) => {
-    const atendimento = atendimento_controller.show(req.params.id);
-    res.json(atendimento);
-})
-
-app.put("/atendimento/:id", (req, res) => {
-    const atendimento = req.body;
-    const code = atendimento_controller.update(req.params.id, atendimento);
-    res.status(code).json();
-})
-
-app.delete("/atendimento/:id", (req, res) => {
-    atendimento_controller.destroy(req.params.id);
+app.post("/barbearia", (req, res) => {
+    console.log(req.body);
     res.json();
+})
+
+app.get("/barbearia", (req, res) => {
+    res.json()
 })
 
 //PORTA
